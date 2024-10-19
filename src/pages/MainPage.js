0f7cfae5e6a0bee19b340d2ca14f7a4745ea3497
leevscode <leevscode@gmail.com>
2024-10-19 19:33:06 +0900

    feat: 이미지 슬라이드 테스트 이미지 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,58 @@
-import React from "react";
-import SightseeingPage from "./SightseeingPage";
+import React, { useState, useEffect } from "react";
+import "./MainPage.css";
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const images = [
+    "https://via.placeholder.com/800x300?text=Image+1",
+    "https://via.placeholder.com/800x300?text=Image+2",
+    "https://via.placeholder.com/800x300?text=Image+3",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+    // 3초마다 슬라이드 자동 전환, 추후 수정 예정
+    return () => clearInterval(timer);
+  }, [images.length]);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  return (
+    <div className="slider-container">
+      <div className="image-wrapper">
+        <button onClick={prevSlide} className="button button-prev">
+          {"<"}
+        </button>
+        <img
+          src={images[currentIndex]}
+          alt={`slide-${currentIndex}`}
+          className="image"
+        />
+        <button onClick={nextSlide} className="button button-next">
+          {">"}
+        </button>
+      </div>
+      <div>유튜브영상</div>
+
+      <div>대구 테마명소</div>
+
+      <div>대구 추천명소</div>
+
+      <div>대구 TOP명소</div>
+      <div></div>
+    </div>
+  );
 };
 
 export default MainPage;
