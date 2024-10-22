77b6bc5d112e62b5df0de8445d07413419ec6818
leevscode <leevscode@gmail.com>
2024-10-22 16:17:55 +0900

    fix: 이미지 슬라이드 버튼 수정

---
+++ src/components/ThemeBanner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ThemeBanner = () => {
+  return (
+    <div>ThemeBanner</div>
+  )
+}
+
+export default ThemeBanner
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,13 +2,17 @@ import React from "react";
 import ImageSlider from "../components/ImageSlider";
 import "./MainPage.css";
 import YouTubeVideo from "../components/YouTube";
+import ThemeBanner from "../components/ThemeBanner";
 
 const MainPage = () => {
   return (
     <>
       <ImageSlider />
       <YouTubeVideo />
-      <div>대구 테마명소</div>
+      <div>
+        대구 테마명소
+        <ThemeBanner />
+      </div>
       <div>대구 추천명소</div>
       <div>대구 TOP명소</div>
     </>
