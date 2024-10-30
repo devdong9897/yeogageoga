e32720bbe3327997fdf0f86631cae9548c382031
leevscode <leevscode@gmail.com>
2024-10-30 19:00:14 +0900

    fix: 푸터 css 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  // 헤더도 따로 css를 만들지 않아 여기에 작성 했습니다.
   return (
-    <div style={footerContainerStyle}>
+    <div style={footerStyle}>
       <p style={footerTextStyle}>회사소개 이용약관 개인정보처리방침 고객센터</p>
     </div>
   );
 };
 
-const footerContainerStyle = {
+const footerStyle = {
   backgroundColor: "#e0e0e0",
-  padding: "40px 310px",
+  padding: "40px 300px",
 };
 
 const footerTextStyle = {
@@ -19,6 +18,7 @@ const footerTextStyle = {
   fontSize: "18px",
   letterSpacing: "0.5px",
   margin: "0",
+  textAlign: "center",
 };
 
 export default Footer;
