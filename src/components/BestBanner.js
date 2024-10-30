851011cf81bd99de15ccfaeae54322aed6ddf715
leevscode <leevscode@gmail.com>
2024-10-30 17:42:10 +0900

    fix: 슬라이드 네비게이션 바 버그 수정

--- src/components/BestBanner.js
+++ src/components/BestBanner.js
@@ -3,56 +3,80 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "../pages/MainPage.css";
+import { useRef } from "react";
 
 const BestBanner = () => {
+  const swiperRef = useRef(null);
+  // 버튼을 독립적으로 움직이기 위해 useRef를 사용
+
+  const bestData = [
+    {
+      image: "/images/test1.jpg",
+      placeName: "장소1",
+      tourType: "카페 투어 중",
+      date: "2023년 12월 27일",
+    },
+    {
+      image: "/images/test2.jpg",
+      placeName: "장소2",
+      tourType: "유원지 투어",
+      date: "2024년 1월 10일",
+    },
+    {
+      image: "/images/test3.jpg",
+      placeName: "장소3",
+      tourType: "도시 탐방",
+      date: "2024년 2월 14일",
+    },
+    {
+      image: "/images/test1.jpg",
+      placeName: "장소4",
+      tourType: "힐링 투어",
+      date: "2024년 3월 1일",
+    },
+    {
+      image: "/images/test2.jpg",
+      placeName: "장소5",
+      tourType: "문화 유적 투어",
+      date: "2024년 4월 5일",
+    },
+  ];
+
   return (
     <div className="banner-container">
       <Swiper
+        ref={swiperRef}
         spaceBetween={50}
         slidesPerView={4}
         loop={true}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
         navigation={{
-          nextEl: ".best-button-next",
-          prevEl: ".best-button-prev",
+          nextEl: ".button-next-best",
+          prevEl: ".button-prev-best",
         }}
         modules={[Navigation]}
       >
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test1.jpg" alt="Slide 1" className="image" />
-            <div className="place-text">테스트 1</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test2.jpg" alt="Slide 2" className="image" />
-            <div className="place-text">테스트 2</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test3.jpg" alt="Slide 3" className="image" />
-            <div className="place-text">테스트 3</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test1.jpg" alt="Slide 4" className="image" />
-            <div className="place-text">테스트 4</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test2.jpg" alt="Slide 5" className="image" />
-            <div className="place-text">테스트 5</div>
-          </div>
-        </SwiperSlide>
+        {bestData.map((best, index) => (
+          <SwiperSlide key={index}>
+            <div className="card">
+              <img
+                src={best.image}
+                alt={`Slide ${index + 1}`}
+                className="card-image"
+              />
+              <div className="card-content">
+                <div className="place-name">{best.placeName}</div>
+                <div className="info-container">
+                  <div className="tour-type">{best.tourType}</div>
+                  <div className="date">{best.date}</div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
       {/* 네비게이션 버튼 */}
-      <button className="button best-button-prev">{"<"}</button>
-      <button className="button best-button-next">{">"}</button>
+      <button className="button button-prev-best">{"<"}</button>
+      <button className="button button-next-best">{">"}</button>
     </div>
   );
 };
--- src/components/ThemeBanner.js
+++ src/components/ThemeBanner.js
@@ -3,56 +3,80 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "../pages/MainPage.css";
+import { useRef } from "react";
 
 const ThemeBanner = () => {
+  const swiperRef = useRef(null);
+  // 버튼을 독립적으로 움직이기 위해 useRef를 사용
+
+  const themeData = [
+    {
+      image: "/images/test1.jpg",
+      placeName: "장소1",
+      tourType: "카페 투어 중",
+      date: "2023년 12월 27일",
+    },
+    {
+      image: "/images/test2.jpg",
+      placeName: "장소2",
+      tourType: "유원지 투어",
+      date: "2024년 1월 10일",
+    },
+    {
+      image: "/images/test3.jpg",
+      placeName: "장소3",
+      tourType: "도시 탐방",
+      date: "2024년 2월 14일",
+    },
+    {
+      image: "/images/test1.jpg",
+      placeName: "장소4",
+      tourType: "힐링 투어",
+      date: "2024년 3월 1일",
+    },
+    {
+      image: "/images/test2.jpg",
+      placeName: "장소5",
+      tourType: "문화 유적 투어",
+      date: "2024년 4월 5일",
+    },
+  ];
+
   return (
     <div className="banner-container">
       <Swiper
+        ref={swiperRef}
         spaceBetween={50}
         slidesPerView={4}
-        loop={true} // 무한 스와이프 활성화
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        loop={true}
         navigation={{
-          nextEl: ".button-next",
-          prevEl: ".button-prev",
+          nextEl: ".button-next-theme",
+          prevEl: ".button-prev-theme",
         }}
         modules={[Navigation]}
       >
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test1.jpg" alt="Slide 1" className="image" />
-            <div className="place-text">테스트 1</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test2.jpg" alt="Slide 2" className="image" />
-            <div className="place-text">테스트 2</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test3.jpg" alt="Slide 3" className="image" />
-            <div className="place-text">테스트 3</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test1.jpg" alt="Slide 4" className="image" />
-            <div className="place-text">테스트 4</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="image-wrapper">
-            <img src="/images/test2.jpg" alt="Slide 5" className="image" />
-            <div className="place-text">테스트 5</div>
-          </div>
-        </SwiperSlide>
+        {themeData.map((theme, index) => (
+          <SwiperSlide key={index}>
+            <div className="card">
+              <img
+                src={theme.image}
+                alt={`Slide ${index + 1}`}
+                className="card-image"
+              />
+              <div className="card-content">
+                <div className="place-name">{theme.placeName}</div>
+                <div className="info-container">
+                  <div className="tour-type">{theme.tourType}</div>
+                  <div className="date">{theme.date}</div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
       {/* 네비게이션 버튼 */}
-      <button className="button button-prev">{"<"}</button>
-      <button className="button button-next">{">"}</button>
+      <button className="button button-prev-theme">{"<"}</button>
+      <button className="button button-next-theme">{">"}</button>
     </div>
   );
 };
